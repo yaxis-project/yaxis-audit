c7c9837e539b046b0f7ca55fd2f672b295dd7048
yaxis.dev <yaxis.dev@protonmail.com>
2020-09-17 11:33:25 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# audit
+https://yaxis.io/
---
+++ contracts/YaxisChef.sol
@@ -0,0 +1,309 @@
+pragma solidity 0.6.12;
+
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
+import "@openzeppelin/contracts/utils/EnumerableSet.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "./YaxisToken.sol";
+
+
+interface IMigratorChef {
+    // Perform LP token migration from legacy UniswapV2 to yAxis.
+    // Take the current LP token address and return the new LP token address.
+    // Migrator should have full access to the caller's LP token.
+    // Return the new LP token address.
+    //
+    // XXX Migrator must have allowance access to UniswapV2 LP tokens.
+    // yAxis must mint EXACTLY the same amount of yAxis LP tokens or
+    // else something bad will happen. Traditional UniswapV2 does not
+    // do that so be careful!
+    function migrate(IERC20 token) external returns (IERC20);
+}
+
+// YaxisChef is the master of yAxis. He can make yAxis and he is a fair guy.
+//
+// Note that it's ownable and the owner wields tremendous power. The ownership
+// will be transferred to a governance smart contract once YAX is sufficiently
+// distributed and the community can show to govern itself.
+//
+// Have fun reading it. Hopefully it's bug-free. God bless.
+contract YaxisChef is Ownable {
+    using SafeMath for uint256;
+    using SafeERC20 for IERC20;
+
+    // Info of each user.
+    struct UserInfo {
+        uint256 amount;     // How many LP tokens the user has provided.
+        uint256 rewardDebt; // Reward debt. See explanation below.
+        //
+        // We do some fancy math here. Basically, any point in time, the amount of YAXs
+        // entitled to a user but is pending to be distributed is:
+        //
+        //   pending reward = (user.amount * pool.accYaxPerShare) - user.rewardDebt
+        //
+        // Whenever a user deposits or withdraws LP tokens to a pool. Here's what happens:
+        //   1. The pool's `accYaxPerShare` (and `lastRewardBlock`) gets updated.
+        //   2. User receives the pending reward sent to his/her address.
+        //   3. User's `amount` gets updated.
+        //   4. User's `rewardDebt` gets updated.
+    }
+
+    // Info of each pool.
+    struct PoolInfo {
+        IERC20 lpToken;           // Address of LP token contract.
+        uint256 allocPoint;       // How many allocation points assigned to this pool. YAXs to distribute per block.
+        uint256 lastRewardBlock;  // Last block number that YAXs distribution occurs.
+        uint256 accYaxPerShare; // Accumulated YAXs per share, times 1e12. See below.
+    }
+
+    // The YAX TOKEN!
+    YaxisToken public yax;
+    // Tresury address.
+    address public tresuryaddr;
+    // YAX tokens created per block.
+    uint256 public yaxPerBlock;
+    // The migrator contract. It has a lot of power. Can only be set through governance (owner).
+    IMigratorChef public migrator;
+
+    // Info of each pool.
+    PoolInfo[] public poolInfo;
+    // Info of each user that stakes LP tokens.
+    mapping (uint256 => mapping (address => UserInfo)) public userInfo;
+    // Total allocation poitns. Must be the sum of all allocation points in all pools.
+    uint256 public totalAllocPoint = 0;
+    // The block number when YAX mining starts.
+    uint256 public startBlock;
+    // Block number when each epoch ends.
+    uint256[4] public epochEndBlocks;
+
+    // Reward multipler for each of 4 epoches (epochIndex: reward multipler)
+    uint256[5] public epochRewardMultiplers = [3840, 2880, 1920, 960, 1];
+
+    uint256 public constant BLOCKS_PER_WEEK = 46500;
+
+    event Deposit(address indexed user, uint256 indexed pid, uint256 amount);
+    event Withdraw(address indexed user, uint256 indexed pid, uint256 amount);
+    event EmergencyWithdraw(address indexed user, uint256 indexed pid, uint256 amount);
+
+    constructor(
+        YaxisToken _yax,
+        address _tresuryaddr,
+        uint256 _yaxPerBlock,
+        uint256 _startBlock
+    ) public {
+        yax = _yax;
+        tresuryaddr = _tresuryaddr;
+        yaxPerBlock = _yaxPerBlock; // supposed to be 0.001 (1e16 wei)
+        startBlock = _startBlock; // supposed to be 10,883,800 (Fri Sep 18 2020 3:00:00 GMT+0)
+        epochEndBlocks[0] = startBlock + BLOCKS_PER_WEEK * 2; // weeks 1-2
+        epochEndBlocks[1] = epochEndBlocks[0] + BLOCKS_PER_WEEK * 2; // weeks 3-4
+        epochEndBlocks[2] = epochEndBlocks[1] + BLOCKS_PER_WEEK * 2; // weeks 5-6
+        epochEndBlocks[3] = epochEndBlocks[2] + BLOCKS_PER_WEEK * 2; // weeks 7-8
+    }
+
+    function poolLength() external view returns (uint256) {
+        return poolInfo.length;
+    }
+
+    // Add a new lp to the pool. Can only be called by the owner.
+    // XXX DO NOT add the same LP token more than once. Rewards will be messed up if you do.
+    function add(uint256 _allocPoint, IERC20 _lpToken, bool _withUpdate) public onlyOwner {
+        if (_withUpdate) {
+            massUpdatePools();
+        }
+        uint256 _lastRewardBlock = block.number > startBlock ? block.number : startBlock;
+        totalAllocPoint = totalAllocPoint.add(_allocPoint);
+        poolInfo.push(PoolInfo({
+            lpToken: _lpToken,
+            allocPoint: _allocPoint,
+            lastRewardBlock: _lastRewardBlock,
+            accYaxPerShare: 0
+        }));
+    }
+
+    function setYaxisPerBlock(uint256 _yaxPerBlock) public onlyOwner {
+        massUpdatePools();
+        yaxPerBlock = _yaxPerBlock;
+    }
+
+    function setEpochEndBlock(uint8 _index, uint256 _epochEndBlock) public onlyOwner {
+        require(_index < 4, "_index out of range");
+        require(epochEndBlocks[_index] > block.number, "Too late to update");
+        epochEndBlocks[_index] = _epochEndBlock;
+    }
+
+    function setEpochRewardMultipler(uint8 _index, uint256 _epochRewardMultipler) public onlyOwner {
+        require(_index < 5, "Index out of range");
+        if (_index < 4) {
+            require(epochEndBlocks[_index] > block.number, "Too late to update");
+        }
+        epochRewardMultiplers[_index] = _epochRewardMultipler;
+    }
+
+    // Update the given pool's YAX allocation point. Can only be called by the owner.
+    function set(uint256 _pid, uint256 _allocPoint, bool _withUpdate) public onlyOwner {
+        if (_withUpdate) {
+            massUpdatePools();
+        }
+        totalAllocPoint = totalAllocPoint.sub(poolInfo[_pid].allocPoint).add(_allocPoint);
+        poolInfo[_pid].allocPoint = _allocPoint;
+    }
+
+    // Set the migrator contract. Can only be called by the owner.
+    function setMigrator(IMigratorChef _migrator) public onlyOwner {
+        migrator = _migrator;
+    }
+
+    // Migrate lp token to another lp contract. Can be called by anyone. We trust that migrator contract is good.
+    function migrate(uint256 _pid) public {
+        require(address(migrator) != address(0), "migrate: no migrator");
+        PoolInfo storage pool = poolInfo[_pid];
+        IERC20 lpToken = pool.lpToken;
+        uint256 bal = lpToken.balanceOf(address(this));
+        lpToken.safeApprove(address(migrator), bal);
+        IERC20 newLpToken = migrator.migrate(lpToken);
+        require(bal == newLpToken.balanceOf(address(this)), "migrate: bad");
+        pool.lpToken = newLpToken;
+    }
+
+    // Return reward multiplier over the given _from to _to block.
+    function getMultiplier(uint256 _from, uint256 _to) public view returns (uint256) {
+        for (uint8 epochId = 4; epochId >= 1; --epochId) {
+            if (_to >= epochEndBlocks[epochId - 1]) {
+                if (_from >= epochEndBlocks[epochId - 1]) return _to.sub(_from).mul(epochRewardMultiplers[epochId]);
+                uint256 multiplier = _to.sub(epochEndBlocks[epochId - 1]).mul(epochRewardMultiplers[epochId]);
+                if (epochId == 1) return multiplier.add(epochEndBlocks[0].sub(_from).mul(epochRewardMultiplers[0]));
+                for (epochId = epochId - 1; epochId >= 1; --epochId) {
+                    if (_from >= epochEndBlocks[epochId - 1]) return multiplier.add(epochEndBlocks[epochId].sub(_from).mul(epochRewardMultiplers[epochId]));
+                    multiplier = multiplier.add(epochEndBlocks[epochId].sub(epochEndBlocks[epochId - 1]).mul(epochRewardMultiplers[epochId]));
+                }
+                return multiplier.add(epochEndBlocks[0].sub(_from).mul(epochRewardMultiplers[0]));
+            }
+        }
+        return _to.sub(_from).mul(epochRewardMultiplers[0]);
+    }
+
+    // View function to see pending YAXs on frontend.
+    function pendingYaxis(uint256 _pid, address _user) external view returns (uint256) {
+        PoolInfo storage pool = poolInfo[_pid];
+        UserInfo storage user = userInfo[_pid][_user];
+        uint256 accYaxPerShare = pool.accYaxPerShare;
+        uint256 lpSupply = pool.lpToken.balanceOf(address(this));
+        if (block.number > pool.lastRewardBlock && lpSupply != 0) {
+            uint256 multiplier = getMultiplier(pool.lastRewardBlock, block.number);
+            uint256 yaxReward = multiplier.mul(yaxPerBlock).mul(pool.allocPoint).div(totalAllocPoint);
+            accYaxPerShare = accYaxPerShare.add(yaxReward.mul(1e12).div(lpSupply));
+        }
+        return user.amount.mul(accYaxPerShare).div(1e12).sub(user.rewardDebt);
+    }
+
+    // Update reward variables for all pools. Be careful of gas spending!
+    function massUpdatePools() public {
+        uint256 length = poolInfo.length;
+        for (uint256 pid = 0; pid < length; ++pid) {
+            updatePool(pid);
+        }
+    }
+
+    // Update reward variables of the given pool to be up-to-date.
+    function updatePool(uint256 _pid) public {
+        PoolInfo storage pool = poolInfo[_pid];
+        if (block.number <= pool.lastRewardBlock) {
+            return;
+        }
+        uint256 lpSupply = pool.lpToken.balanceOf(address(this));
+        if (lpSupply == 0) {
+            pool.lastRewardBlock = block.number;
+            return;
+        }
+        uint256 multiplier = getMultiplier(pool.lastRewardBlock, block.number);
+        uint256 yaxReward = multiplier.mul(yaxPerBlock).mul(pool.allocPoint).div(totalAllocPoint);
+        safeYaxMint(tresuryaddr, yaxReward.div(9));
+        safeYaxMint(address(this), yaxReward);
+        pool.accYaxPerShare = pool.accYaxPerShare.add(yaxReward.mul(1e12).div(lpSupply));
+        pool.lastRewardBlock = block.number;
+    }
+
+    // Deposit LP tokens to YaxisChef for YAX allocation.
+    function deposit(uint256 _pid, uint256 _amount, address _referrer) public {
+        PoolInfo storage pool = poolInfo[_pid];
+        UserInfo storage user = userInfo[_pid][msg.sender];
+        updatePool(_pid);
+        if (user.amount > 0) {
+            uint256 pending = user.amount.mul(pool.accYaxPerShare).div(1e12).sub(user.rewardDebt);
+            if(pending > 0) {
+                safeYaxTransfer(msg.sender, pending);
+            }
+        }
+        if(_amount > 0) {
+            pool.lpToken.safeTransferFrom(address(msg.sender), address(this), _amount);
+            user.amount = user.amount.add(_amount);
+        }
+        user.rewardDebt = user.amount.mul(pool.accYaxPerShare).div(1e12);
+        emit Deposit(msg.sender, _pid, _amount);
+    }
+
+    // Withdraw LP tokens from YaxisChef.
+    function withdraw(uint256 _pid, uint256 _amount) public {
+        PoolInfo storage pool = poolInfo[_pid];
+        UserInfo storage user = userInfo[_pid][msg.sender];
+        require(user.amount >= _amount, "withdraw: not good");
+        updatePool(_pid);
+        uint256 pending = user.amount.mul(pool.accYaxPerShare).div(1e12).sub(user.rewardDebt);
+        if(pending > 0) {
+            safeYaxTransfer(msg.sender, pending);
+        }
+        if(_amount > 0) {
+            user.amount = user.amount.sub(_amount);
+            pool.lpToken.safeTransfer(address(msg.sender), _amount);
+        }
+        user.rewardDebt = user.amount.mul(pool.accYaxPerShare).div(1e12);
+        emit Withdraw(msg.sender, _pid, _amount);
+    }
+
+    // Withdraw without caring about rewards. EMERGENCY ONLY.
+    function emergencyWithdraw(uint256 _pid) public {
+        PoolInfo storage pool = poolInfo[_pid];
+        UserInfo storage user = userInfo[_pid][msg.sender];
+        pool.lpToken.safeTransfer(address(msg.sender), user.amount);
+        emit EmergencyWithdraw(msg.sender, _pid, user.amount);
+        user.amount = 0;
+        user.rewardDebt = 0;
+    }
+
+    // Safe yax mint, ensure it is never over cap and we are the current owner.
+    function safeYaxMint(address _to, uint256 _amount) internal {
+        if (yax.governance() == address(this) && _to != address(0)) {
+            uint256 totalSupply = yax.totalSupply();
+            uint256 cap = yax.cap();
+            if (totalSupply.add(_amount) > cap) {
+                yax.mint(_to, cap.sub(totalSupply));
+            } else {
+                yax.mint(_to, _amount);
+            }
+        }
+    }
+
+    // Safe yax transfer function, just in case if rounding error causes pool to not have enough YAXs.
+    function safeYaxTransfer(address _to, uint256 _amount) internal {
+        uint256 yaxBal = yax.balanceOf(address(this));
+        if (_amount > yaxBal) {
+            yax.transfer(_to, yaxBal);
+        } else {
+            yax.transfer(_to, _amount);
+        }
+    }
+
+    // Update tresury by the previous tresury contract.
+    function tresury(address _tresuryaddr) public {
+        require(msg.sender == tresuryaddr, "tresury: wut?");
+        tresuryaddr = _tresuryaddr;
+    }
+
+    // Transfer ownership of YaxisToken (eg. to a governance DAO contract)
+    function transferYaxisTokenOwnership(address newYaxisOwner) public onlyOwner {
+        YaxisToken(yax).transferOwnership(newYaxisOwner);
+    }
+}
